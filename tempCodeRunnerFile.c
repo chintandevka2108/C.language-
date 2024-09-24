56c97a1e6ad57bdc07a2ebfb53e91d02a949db82
chitandevka2108 <chitangadhavi511@gmail.com>
2024-09-24 18:00:18 +0530

    Program

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+ float a;
+
+     printf("Enter a number:");
+    scanf("%f", &a);
+
+    if(a>0){
+        printf("number is positive");
+    }
+    else if(a<0){
+        printf("number is negative");
+    }
+    else{
+        printf("neutral");
+    }
