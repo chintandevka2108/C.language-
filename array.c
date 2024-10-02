2987da780a55a576f7bb8ebd3ac27d7224b3b28e
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-02 12:52:50 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+ int main() 
+{
+  ///**********Q.1**********///
+  // int i,b;
+  // int a[10];
+  // 
+  // printf("enter array size : ");
+  // scanf("%d",&b);
+  // printf("enter the element in the array : \n");
+  // for ( i = 0; i < b; i++)
+  // {
+    // printf("a[%d]:",i);
+    // scanf("%d",a[i]);
+  // }
+  // printf("The Length of the array is : %d",b);
+
+    ///**********Q.2**********/// 
+    //  int i,b;
+    //  float sum = 0;
+    //  int a[10];
+    // 
+    // printf("enter array size : ");
+    // scanf("%d",&b);
+    // printf("enter the element in the array : \n");
+    // for ( i = 0; i < b; i++)
+  // {
+  //  printf("a[%d]: ",i);
+  //  scanf("%d",&a[i]);
+  //  sum = sum + a[i];
+  // }
+    // float avg = sum/b;
+
+    //  printf("The Length of the array is : %f",avg);
+
+    ///**********Q.3**********/// 
+
+    //  int i,n;
+  
+   // printf("enter array size : ");
+  // scanf("%d",&n);
+  // int a[n],b[n],c[n];
+  // printf("enter array 'A's element : \n");
+  // for ( i = 0; i < n; i++)
+    // {
+  // printf("a[%d]= ",i);
+  // scanf("%d",&a[i]);
+    // }
+  // printf("enter array 'B's element : \n");
+    // for( i = 0; i < n; i++)
+  // {  printf("b[%d]= ",i);
+   //  scanf("%d",&b[i]);
+  // }
+   // for( i = 0; i < n; i++)
+  // {
+  // c[i]= a[i]+b[i];
+  // }
+
+  //  printf("enter array 'C' is  : \n");
+//  for( i = 0; i < n; i++)
+  //  printf("%d",c[i]);
+  //  if(i < n-1);
+  //  {
+  //  printf(",");
+//  }
+  //  printf("\n");
+
+
+
+    return 0;
+}
