3849b9c45b37a3b30364c45dbc5a03b3344574cb
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-03 11:23:41 +0530

    Add files via upload

---
+++ arrayquestion.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+int main() {
+
+    ///**********Q.1**********///
+
+    // int n;
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &n);
+
+    // int arr[n];
+
+    // printf("Enter %d elements: \n", n);
+    // for(int i = 0; i < n; i++) {
+        // scanf("%d", &arr[i]);
+    // }
+    // printf("The array elements are: \n");
+    // for(int i = 0; i < n; i++) {
+        // printf("%d ", arr[i]; 
+
+
+
+     ///**********Q.2**********///
+
+    // int n, sum = 0;
+
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &n);
+
+    // int arr[n]; 
+    // printf("Enter %d elements: \n", n);
+    // for(int i = 0; i < n; i++) {
+        // scanf("%d", &arr[i]);
+    // }
+
+ 
+    // for(int i = 0; i < n; i++) {
+        // sum += arr[i];
+    // }
+//    printf("The sum of all elements is: %d\n", sum);
+
+
+
+
+     ///**********Q.3**********///
+// 
+    //   int n, sum = 0;
+// 
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &n);
+// 
+    // int arr[n]; 
+// 
+    // printf("Enter %d elements: \n", n);
+    // for(int i = 0; i < n; i++) {
+        // scanf("%d", &arr[i]);
+    // }
+    // for(int i = 0; i < n; i += 2) {
+        // sum += arr[i];
+    // }
+    // printf("The sum of elements at even indexes is: %d\n", sum);
+// 
+
+     ///**********Q.4**********///
+
+// 
+    //  int n, max, min;
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &n);
+// 
+    // int arr[n]; 
+    // printf("Enter %d elements: \n", n);
+    // for(int i = 0; i < n; i++) {
+        // scanf("%d", &arr[i]);
+    // }
+    // max = min = arr[0];
+    // for(int i = 1; i < n; i++) {
+        // if(arr[i] > max) {
+            // max = arr[i];
+        // }
+        // if(arr[i] < min) {
+            // min = arr[i];
+        // }
+    // }
+    // printf("Maximum value is: %d\n", max);
+    // printf("Minimum value is: %d\n", min);
+// 
+// 
+
+    return 0;
+}
