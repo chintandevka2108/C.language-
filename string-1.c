8e88528916a9c881998e85732afcaf7287785b18
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-07 18:55:22 +0530

    Add files via upload

---
+++ string-1.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main (){
+
+
+  ///***********************Q.1***********************///
+
+    // char str[100];
+    // int i;
+
+    // printf("Enter any string: ");
+    // gets(str);
+
+    // for(i = 0; str[i] != '\0'; i++) {
+        // if(str[i] >= 'a' && str[i] <= 'z') {
+            // str[i] = str[i] - 32;
+        // }
+    // }
+
+    // printf("Uppercase string: %s\n", str);
+
+
+
+  ///***********************Q.2***********************///
+
+  // char str[100];
+//  int i;
+//  printf("Enter any string: ");
+//  gets(str);
+//  for(i = 0; str[i] != '\0'; i++) {
+    //  if(str[i] >= 'A' && str[i] <= 'Z') {
+        //  str[i] = str[i] + 32;
+    //  }
+//  }
+//  printf("Lowercase string: %s\n", str);
+
+
+  ///***********************Q.2***********************///
+
+
+//  char str[100];
+//  int i;
+//  printf("Enter any string: ");
+//  gets(str);
+//  for(i = 0; str[i] != '\0'; i++) {
+    //  if(str[i] >= 'a' && str[i] <= 'z') {
+        //  str[i] = str[i] - 32; 
+    //  } else if(str[i] >= 'A' && str[i] <= 'Z') {
+        //  str[i] = str[i] + 32; 
+    //  }
+//  }
+//  printf("Toggle case string: %s\n", str);
+// 
+
+    return 0;
+}
+
