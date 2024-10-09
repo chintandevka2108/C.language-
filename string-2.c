8d20315d43bbc4bc8cfbd04fd28f0cf3171bb63b
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-09 11:06:28 +0530

    Add files via upload

---
+++ string-2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+
+      ///***********************Q.1***********************///
+// 
+    // char pswd[50];
+    // int l=0, d=0, s=0, len=0;
+    // 
+    // printf("Create your password: ");
+    // scanf("%s", pswd);
+    // 
+    // while (pswd[len] != '\0') {
+        // if ((pswd[len] >= 'a' && pswd[len] <= 'z') || (pswd[len] >= 'A' && pswd[len] <= 'Z')) l = 1;
+        // else if (pswd[len] >= '0' && pswd[len] <= '9') d = 1;
+        // else s = 1;
+        // len++;
+    // }
+    // 
+    // if (len >= 6 && l && d && s) printf("Your password is Strong.\n");
+    // else printf("Your password is not Strong.\n");
+// 
+// 
+      ///***********************Q.2***********************///
+
+
+    //    char email[50], password[50];
+    //   char correct_email[] = "admin@gmail.com";
+    //   char correct_password[] = "123456";
+    //   int i = 0, email_match = 1, password_match = 1;
+    //   printf("Enter your email: ");
+    //   scanf("%s", email);
+    //   printf("Enter your password: ");
+    //   scanf("%s", password);
+    //   while (email[i] != '\0' || correct_email[i] != '\0') 
+    //   {
+        //   if (email[i] != correct_email[i]) 
+        //   {
+            //   email_match = 0;
+            //   break;
+        //   }
+        //   i++;
+    //   }
+    //   
+    //   i = 0;
+    //   while (password[i] != '\0' || correct_password[i] != '\0') 
+    //   {
+        //   if (password[i] != correct_password[i]) 
+        //   {
+            //   password_match = 0;
+            //   break;
+        //   }
+        //   i++;
+    //   }
+    //   if (email_match && password_match) 
+    //   {
+        //   printf("Login Successful...\n");
+    //   } else 
+    //   {
+        //   printf("Login Failed. Invalid Credentials.\n");
+    //   }
+   
+       return 0;
+}
