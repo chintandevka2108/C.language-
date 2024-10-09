71dd0254464352f6eeb7eaeadbdab2b277d5e01f
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-09 11:05:40 +0530

    Add files via upload

---
+++ array and string.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main() {
+
+
+    ///***********************Q.1***********************///
+      
+        //    int n, i, tmp;
+        // printf("Enter size: ");
+        // scanf("%d", &n);
+        // int arr[n];
+        // 
+        // for(i = 0; i < n; i++)
+            // scanf("%d", &arr[i]);
+        // 
+        // for(i = 0; i < n/2; i++) {
+            // tmp = arr[i];
+            // arr[i] = arr[n-i-1];
+            // arr[n-i-1] = tmp;
+        // }
+        // 
+        // for(i = 0; i < n; i++)
+            // printf("%d ", arr[i]);
+      
+
+  ///***********************Q.2***********************///
+       
+        // int n, i, even = 0, odd = 0;
+    //    printf("Enter size: ");
+    //    scanf("%d", &n);
+    //    int arr[n];
+        //    
+    //    for(i = 0; i < n; i++) {
+        //    scanf("%d", &arr[i]);
+        //    if(arr[i] % 2 == 0)
+            //    even++;
+        //    else
+            //    odd++;
+    //    }
+        //    
+    //    printf("Even: %d\n", even);
+    //    printf("Odd: %d\n", odd);
+           
+           
+        ///***********************Q.3***********************///
+
+        // char str[100];
+        // int i, v = 0, c = 0;
+        // printf("Enter a string: ");
+        // gets(str);
+            // 
+        // for(i = 0; str[i] != '\0'; i++) {
+            // if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+                // if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || 
+                //    str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                    // v++;
+                // else
+                    // c++;
+            // }
+        // }
+            // 
+        // printf("Vowels: %d\n", v);
+        // printf("Consonants: %d\n", c);
+      
+    
+
+
+    return 0;
+}
