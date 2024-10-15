417b346fb88588d9e39098112f99758c1ec29032
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-15 19:29:49 +0530

    Add files via upload

---
+++ filament.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+int main(){
+
+
+    ///*****************Q-1*********************///
+
+//  char str[100];
+//  int start, end, flag = 1;
+//  printf("Enter any string: ");
+//  scanf("%s", str);
+//  for (end = 0; str[end] != '\0'; end++);
+//  end--;
+//  for (start = 0; start < end; start++, end--) {
+    //  if (str[start] != str[end]) {
+        //  flag = 0;
+        //  break;
+    //  }
+//  }
+//  if (flag)
+    //  printf("The given string is a Palindrome.\n");
+//  else
+    //  printf("The given string is not a Palindrome.\n");
+//  return 0;
+//  char str[100];
+//  int start, end, flag = 1;
+//  printf("Enter any string: ");
+//  scanf("%s", str);
+//  for (end = 0; str[end] != '\0'; end++);
+//  end--;
+//  for (start = 0; start < end; start++, end--) {
+    //  if (str[start] != str[end]) {
+        //  flag = 0;
+        //  break;
+    //  }
+//  }
+//  if (flag)
+    //  printf("The given string is a Palindrome.\n");
+//  else
+    //  printf("The given string is not a Palindrome.\n");
+//  return 0;
+//  char str[100];
+//  int start, end, flag = 1;
+//  printf("Enter any string: ");
+//  scanf("%s", str);
+//  for (end = 0; str[end] != '\0'; end++);
+//  end--;
+//  for (start = 0; start < end; start++, end--) {
+    //  if (str[start] != str[end]) {
+        //  flag = 0;
+        //  break;
+    //  }
+//  }
+//  if (flag)
+    //  printf("The given string is a Palindrome.\n");
+//  else
+    //  printf("The given string is not a Palindrome.\n");
+//  return 0;
+//  char str[100];
+//  int start, end, flag = 1;
+//  printf("Enter any string: ");
+//  scanf("%s", str);
+//  for (end = 0; str[end] != '\0'; end++);
+//  end--;
+//  for (start = 0; start < end; start++, end--) {
+    //  if (str[start] != str[end]) {
+        //  flag = 0;
+        //  break;
+    //  }
+//  }
+//  if (flag)
+    //  printf("The given string is a Palindrome.\n");
+//  else
+    //  printf("The given string is not a Palindrome.\n");
+//  return 0;
+//  char str[100];
+//  int start, end, flag = 1;
+//  printf("Enter any string: ");
+//  scanf("%s", str);
+//  for (end = 0; str[end] != '\0'; end++);
+//  end--;
+//  for (start = 0; start < end; start++, end--) {
+    //  if (str[start] != str[end]) {
+        //  flag = 0;
+        //  break;
+    //  }
+//  }
+//  if (flag)
+    //  printf("The given string is a Palindrome.\n");
+//  else
+    //  printf("The given string is not a Palindrome.\n");
+//  return 0;
+
+
+
+        ///*****************Q-2*********************///
+
+
+        //    char str[100];
+    //    int freq[256] = {0};
+    //    int i;
+    //    printf("Enter any string: ");
+    //    scanf("%s", str);
+    //    for (i = 0; str[i] != '\0'; i++)
+        //    freq[(int)str[i]]++;
+    //    printf("Frequency of each letter:\n");
+    //    for (i = 0; i < 256; i++) {
+        //    if (freq[i] > 0)
+            //    printf("%c => %d\n", i, freq[i]);
+    //    }
+
+
+
+}
