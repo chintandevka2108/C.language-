881bba9a3fefceb3cbfb5d750293b2d1b8b9492f
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-21 17:35:58 +0530

    Add files via upload

---
+++ Loops.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+
+///********* Q.1 **********/
+
+   //  int i = 1;
+   //  do {
+   //      printf("%d ", i );
+   //      i++;
+   //  } while (i <= 10);
+
+
+///********* Q.2 **********/
+   
+    // for (int i = 10; i >= 1;i--)
+    // {
+    //      printf("%d ", i );
+    // }
+     
+
+     
+///********* Q.3 **********/
+
+   // int n,i=2;
+
+   // printf("Enter any number:");
+  // scanf("%d",&n);
+
+  // do
+  // {
+ //     printf("%d",i);
+
+  //     i+=2;
+  // } while (i <= n);
+
+
+///********* Q.4 **********/
+
+  // int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+ //     printf("Enter any number: ");
+ //     scanf("%d", &n);
+ 
+ //     printf("%d, %d, ", t1, t2);
+
+  //     for (int i = 3; i <= n; i++) {
+  //         nextTerm = t1 + t2;
+ //         printf("%d, ", nextTerm);
+ //         t1 = t2;
+ //         t2 = nextTerm;
+ //     }
+ //    return 0;
+}
