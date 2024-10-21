5fec7d01336a8d4f09f74443bf3072b84b37a469
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2024-10-21 12:40:28 +0530

    Add files via upload

---
+++ Pointer.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main() {
+
+
+///************Q-1***************///
+
+
+//     // int size, i;
+// // 
+    // printf("Enter the array's size: ");
+    // scanf("%d", &size);
+//     // int arr[size];
+//     // 
+    // for(i = 0; i < size; i++) 
+    // {
+        // printf("a[%d] = ", i);
+
+        // scanf("%d", &arr[i]);
+
+    // }
+//     // int *ptr = arr;  
+// //     // printf("Square of each element: ");
+    // for(i = 0; i < size; i++) 
+    // {
+        // printf("%d", (*(ptr + i)) * (*(ptr + i))); 
+
+        // if (i != size - 1) 
+
+        // {
+
+            // printf(", ");  
+    
+        // }
+
+    // }
+}
+
+///************Q-2***************///
+
+
+// #include <stdio.h>
+// 
+// void swap(int *a, int *b) {
+    // int temp = *a;
+    // *a = *b;
+    // *b = temp;
+// }
+// 
+// int main() {
+    // int x, y;
+// 
+    // printf("Enter the value of x: ");
+    // scanf("%d", &x);
+    // printf("Enter the value of y: ");
+    // scanf("%d", &y);
+// 
+    // printf("Before swapping:\n");
+    // printf("x: %d\n", x);
+    // printf("y: %d\n", y);
+// 
+    // swap(&x, &y);
+// 
+    // printf("After swapping:\n");
+    // printf("x: %d\n", x);
+    // printf("y: %d\n", y);
+// 
+    // return 0;
+// }
+// 
